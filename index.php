6fb65423072d1d1fb8854c2aa0f62a4885591de2
MikePio <michelepiopilla@gmail.com>
2023-05-22 20:16:34 +0200

    Defined class 'Movie' Created php file and readme.md

---
+++ index.php
@@ -0,0 +1,61 @@
+<?php
+
+// definire una classe
+class Movie{
+  public $title;
+  public $filmDuration;
+  public $year;
+}
+
+// attributi/variabili d’istanza
+// var_dump(Movie);
+
+
+
+// Iron Man
+// 2h 5m
+// 2008
+// 
+//Thor
+//1h 54m
+//2011 
+//
+//Avengers: Endgame
+//3h 2m
+//2019 
+//
+//
+
+
+
+
+
+
+?>
+
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- BOOTSTRAP -->
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css' integrity='sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA==' crossorigin='anonymous' />
+  <title>PHP HOTEL</title>
+</head>
+
+<body class="bg-dark text-white">
+  
+  
+  
+</body>
+
+</html>
