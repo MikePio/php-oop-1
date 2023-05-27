cb36840485db810f8f7c0911bef17b4df8413ded
MikePio <michelepiopilla@gmail.com>
2023-05-27 13:16:53 +0200

    Added other movies and card layout improvements

--- index.php
+++ index.php
@@ -153,7 +153,20 @@ class Movie
 $movies = [
   new Movie("img/ironman.jpg", "Iron Man", "2h 5m", "2008", ["Azione", "Fantascienza"]),
   new Movie("img/thor.jpg", "Thor", "1h 54m", "2011", ["Azione", "Avventura", "Fantasy"]),
-  new Movie("img/endgame.jpg", "Avengers: Endgame", "3h 2m", "2019", ["Azione", "Fantascienza", "Supereroi"])
+  new Movie("img/endgame.jpg", "Avengers: Endgame", "3h 2m", "2019", ["Azione", "Fantascienza", "Supereroi"]),
+  new Movie("img/captainamerica.jpg", "Captain America: The First Avenger", "2h 4m", "2011", ["Azione", "Avventura"]),
+  new Movie("img/blackwidow.jpg", "Black Widow", "2h 13m", "2021", ["Azione", "Avventura", "Spionaggio"]),
+  new Movie("img/guardiansofthegalaxy.jpg", "Guardians of the Galaxy", "2h 1m", "2014", ["Azione", "Avventura", "Fantascienza"]),
+  new Movie("img/avengers.jpg", "The Avengers", "2h 23m", "2012", ["Azione", "Fantascienza"]),
+  new Movie("img/captainmarvel.jpg", "Captain Marvel", "2h 3m", "2019", ["Azione", "Fantascienza"]),
+  new Movie("img/antman.jpg", "Ant-Man", "1h 57m", "2015", ["Azione", "Fantascienza"]),
+  new Movie("img/spiderman.jpg", "Spider-Man: Homecoming", "2h 13m", "2017", ["Azione", "Avventura"]),
+  new Movie("img/wintersoldier.jpg", "Captain America: The Winter Soldier", "2h 16m", "2014", ["Azione", "Avventura"]),
+  new Movie("img/doctorstrange.jpg", "Doctor Strange", "1h 55m", "2016", ["Azione", "Fantasy"]),
+  new Movie("img/thor_ragnarok.jpg", "Thor: Ragnarok", "2h 10m", "2017", ["Azione", "Avventura"]),
+  new Movie("img/blackpanther.jpg", "Black Panther", "2h 14m", "2018", ["Azione", "Avventura"]),
+  new Movie("img/civilwar.jpg", "Captain America: Civil War", "2h 27m", "2016", ["Azione", "Fantascienza"]),
+  new Movie("img/loki.jpg", "Loki", "Variabile", "2021", ["Azione", "Fantascienza", "Supereroi"])
 
 ]
 
@@ -187,11 +200,11 @@ $movies = [
     <h1>Marvel Movies</h1>
   </header>
 
-  <div class="container d-flex flex-wrap align-items-center justify-content-between my-5">
+  <div class="container d-flex flex-wrap align-items-center my-5">
 
     <?php foreach ($movies as $movie) : ?>
       <!-- //* stampare con un ciclo foreach -->
-      <div class="text-black my-3" style="width: 275px;">
+      <div class="text-black my-3 ms-4 me-4" style="width: 275px;">
         <div class="card">
           <img src="<?php $movie->getPoster() ?>" class="card-img-top" alt="<?php $movie->getTitle() ?>" style="height: 405px;">
           <div class="card-body">
